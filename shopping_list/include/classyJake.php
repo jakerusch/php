0de08b32a40ca11376f01f2d52317cd4f2edf4e6
Jacob Rusch <Jacob Rusch>
2016-04-10 18:24:08 -0500

    My changes

--- shopping_list/include/classyJake.php
+++ shopping_list/include/classyJake.php
@@ -36,6 +36,7 @@ class classyJake {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 	<meta charset="utf-8">
 	<title>
 END;
