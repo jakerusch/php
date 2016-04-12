8fc6ba3efbedd1b6d79247fb0ccb53fb5f97eca6
Jacob Rusch <Jacob Rusch>
2016-04-12 13:43:16 -0500

    My changes

---
+++ list/temp/insertuser.php
@@ -0,0 +1,22 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT']."/list/include/classyJake.php");
+$obj = new classyJake();
+$conn = $obj->getConn();
+
+// $email=$_POST['email'];
+// $password=$_POST['password'];
+$email="jacob.rusch@gmail.com";
+$password="3stooges";
+$hash=password_hash($password, PASSWORD_DEFAULT);
+
+$sql="INSERT INTO users(user_id, user_password) VALUES ('".$email."', '".$hash."')";
+// $result=$conn->query($sql);
+
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+?>
