a7f7ef6d7a0f88a0e2980723dc6961e87f43613d
jacob.rusch@gmail.com <jacob.rusch@gmail.com>
2017-01-14 10:00:07 -0600

    rolled search code back

--- list/post/search.php
+++ list/post/search.php
@@ -17,6 +17,14 @@ $dropdown = "SELECT item_instances.item_instance_id, item_instances.item_id,
 	INNER JOIN master_locations ON item_instances.location_id=master_locations.location_id
 	INNER JOIN location_instances ON master_locations.location_id=location_instances.location_id
 	WHERE location_instances.location_instance_id='".$id."'
+	AND item_instances.item_instance_id NOT IN
+		(SELECT lists.item_instance_id
+		FROM lists
+		INNER JOIN location_instances ON lists.location_instance_id=location_instances.location_instance_id
+		INNER JOIN master_locations ON location_instances.location_id=master_locations.location_id
+		INNER JOIN item_instances ON lists.item_instance_id=item_instances.item_instance_id
+		INNER JOIN master_items ON item_instances.item_id=master_items.item_id
+		WHERE lists.location_instance_id='".$id."')
 	AND master_items.item_name LIKE '%".$searchTerm."%'
   ORDER BY master_items.item_name ASC;";
 
