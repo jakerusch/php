8016d1953487cafc5ab310302ec0c4190eef2651
jacob.rusch@gmail.com <jacob.rusch@gmail.com>
2017-01-14 09:20:43 -0600

    updated search functionality

--- list/post/search.php
+++ list/post/search.php
@@ -4,13 +4,11 @@ $obj = new classyJake();
 $conn=$obj->getConn();
 $sid=$_SESSION['user_id'];
 $id=$_GET['id'];
-// $sid='1';
-// $id='54';
 
 //get search term
 $searchTerm = $_GET['term'];
 
-//get matched data from skills table
+//get all items possible from particular location_instance_id
 $dropdown = "SELECT item_instances.item_instance_id, item_instances.item_id,
 	item_instances.location_id, item_instances.sort_order, master_items.item_name,
 	location_instances.location_instance_id
@@ -20,15 +18,7 @@ $dropdown = "SELECT item_instances.item_instance_id, item_instances.item_id,
 	INNER JOIN location_instances ON master_locations.location_id=location_instances.location_id
 	WHERE location_instances.location_instance_id='".$id."'
 	AND master_items.item_name LIKE '%".$searchTerm."%'
-    AND item_instances.item_instance_id NOT IN
-	    (SELECT lists.item_instance_id
-		FROM lists
-		INNER JOIN location_instances ON lists.location_instance_id=location_instances.location_instance_id
-		INNER JOIN master_locations ON location_instances.location_id=master_locations.location_id
-		INNER JOIN item_instances ON lists.item_instance_id=item_instances.item_instance_id
-		INNER JOIN master_items ON item_instances.item_id=master_items.item_id
-		WHERE lists.location_instance_id='".$id."')
-    ORDER BY master_items.item_name ASC;";
+  ORDER BY master_items.item_name ASC;";
 
 		$result=$conn->query($dropdown);
 		while($row=$result->fetch_assoc()) {
