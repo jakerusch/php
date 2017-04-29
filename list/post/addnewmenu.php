65317418a9c775e47a77ca89ac36d151456483bd
jacob.rusch@gmail.com <jacob.rusch@gmail.com>
2017-04-29 09:03:09 -0500

    Fixed timezone in meals and insert

--- list/post/addnewmenu.php
+++ list/post/addnewmenu.php
@@ -5,9 +5,13 @@ $conn = $obj->getConn();
 
 $recipe_id=$_POST["recipe_id"];
 
+date_default_timezone_set('America/Chicago');
+$today = new DateTime();
+$today = $today->format('Y-m-d H:i:s');
+
 // creates new list location instance
-$sql = "INSERT INTO recipe_instances(recipe_id)
-  VALUES('$recipe_id')";
+$sql = "INSERT INTO recipe_instances(recipe_id, recipe_timestamp)
+  VALUES('$recipe_id', '$today')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
